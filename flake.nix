2a422754a08ddb3b7a02942a021ae32d21b32257
Mateus Cruz <mateuscolvr@gmail.com>
2023-11-25 01:55:46 -0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nix-ocaml/nix-overlays";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system}.appendOverlays [
+          (self: super: { ocamlPackages = super.ocaml-ng.ocamlPackages_5_1; })
+        ];
+
+        pkgs' = pkgs.pkgsCross.musl64;
+      in {
+        devShells.default = pkgs'.mkShell {
+          nativeBuildInputs = with pkgs'.ocamlPackages; [
+            dune_3
+            findlib
+            ocaml
+            ocaml-lsp
+            ocamlformat
+          ];
+
+          buildInputs = with pkgs'.ocamlPackages; [ ];
+        };
+
+        formatter = pkgs.nixfmt;
+      });
+}
